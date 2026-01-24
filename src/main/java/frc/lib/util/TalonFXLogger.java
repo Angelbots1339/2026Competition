af8e62090595dade2a8739b192f7bcd007f12a2c
dot-Aloysius <tuckercripe10@gmail.com>
2026-01-24 14:08:51 -0700

    Added a Tool to log TalonFX motors

---
+++ src/main/java/frc/lib/util/TalonFXLogger.java
@@ -0,0 +1,27 @@
+package frc.lib.util;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.epilogue.CustomLoggerFor;
+import edu.wpi.first.epilogue.Epilogue;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.logging.ClassSpecificLogger;
+import edu.wpi.first.epilogue.logging.EpilogueBackend;
+
+@CustomLoggerFor(TalonFX.class)
+public class TalonFXLogger extends ClassSpecificLogger<TalonFX>{
+    public TalonFXLogger() {
+        super(TalonFX.class);
+    }
+
+    @Override
+    public void update(EpilogueBackend backend, TalonFX motor) {
+        if (Epilogue.shouldLog(Logged.Importance.DEBUG)) {
+            backend.log("CanID", motor.getDeviceID());
+            backend.log("Applied Voltage (V)", motor.getMotorVoltage().getValueAsDouble());
+            backend.log("Current Velocity (rps)", motor.getVelocity().getValueAsDouble());
+            backend.log("Encoder Position (Rotations)", motor.getPosition().getValueAsDouble());
+            backend.log("Current (A)", motor.getStatorCurrent().getValueAsDouble());
+        }
+    }
+}
