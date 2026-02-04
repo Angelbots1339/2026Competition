f737294d60c4b5b5a2f5f2c91b060eaafa580e93
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-02-04 14:22:05 -0700

    disable unused/unneeded swerve logging

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -24,6 +24,7 @@ import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import choreo.trajectory.SwerveSample;
 import dev.doglog.DogLog;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.epilogue.Logged.Importance;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.VecBuilder;
@@ -32,7 +33,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -49,13 +49,17 @@ import frc.robot.Constants.VisionConstants;
 
 @Logged
 public class Swerve extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> implements Subsystem {
+	@NotLogged
 	private static final double kSimLoopPeriod = 0.004; // 4 ms
+	@NotLogged
 	private Notifier m_simNotifier = null;
 
-	@Logged(name = "Last Sim Time")
+	@NotLogged
 	private double m_lastSimTime;
+	@NotLogged
 	/* Blue alliance sees forward as 0 degrees (toward red alliance wall) */
 	private static final Rotation2d kBlueAlliancePerspectiveRotation = Rotation2d.kZero;
+	@NotLogged
 	/* Red alliance sees forward as 180 degrees (toward blue alliance wall) */
 	private static final Rotation2d kRedAlliancePerspectiveRotation = Rotation2d.k180deg;
 	/* Keep track if we've ever applied the operator perspective before or not */
@@ -334,7 +338,6 @@ public class Swerve extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> impleme
 	@Override
 	public void periodic() {
 		updateVision();
-		updatePID();
 		SmartDashboard.putNumber("distance to Hub",
 				FieldUtil.getHubCenter().getTranslation().getDistance(getPose().getTranslation()));
 
