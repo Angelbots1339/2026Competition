b79bbbefb5c813e8298154dbf3793cc695dd0490
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-02-06 20:57:47 -0700

    remove sysid from shooterTuning

--- src/main/java/frc/lib/util/tuning/ShooterTuning.java
+++ src/main/java/frc/lib/util/tuning/ShooterTuning.java
@@ -1,20 +1,14 @@
 package frc.lib.util.tuning;
 
 import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
-
-import com.ctre.phoenix6.SignalLogger;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Constants.DriverConstants;
 import frc.robot.Constants.TuningConstants;
 import frc.robot.Constants.TuningConstants.TuningMode;
@@ -29,8 +23,6 @@ public class ShooterTuning {
 	private static Trigger runVoltage = baseTrigger.and(() -> tester.getYButton());
 	private static Trigger pidtune = baseTrigger.and(() -> tester.getAButton());
 	private static Trigger pidtuneFOC = baseTrigger.and(() -> tester.getXButton());
-	private static Trigger characterizeVelocity = baseTrigger.and(() -> tester.getBButton());
-	private static SysIdRoutine velocitySysIDRoutine = null;
 
 	private static Shooter shooter;
 
@@ -39,7 +31,6 @@ public class ShooterTuning {
 	public static void init(Shooter shooter) {
 		ShooterTuning.shooter = shooter;
 		shooter.logTuning();
-		setupSysId();
 
 		runVoltage.whileTrue(Commands.run(() -> {
 			shooter.setVoltage();
@@ -54,53 +45,5 @@ public class ShooterTuning {
 		pidtuneFOC.whileTrue(Commands.run(() -> {
 			shooter.setVelocityFOC(targetAngle);
 		}).handleInterrupt(() -> shooter.setVoltage(Volts.of(0))));
-
-		characterizeVelocity.whileTrue(Commands.sequence(
-				Commands.runOnce(SignalLogger::start),
-				sysIdQuasistatic(velocitySysIDRoutine, Direction.kForward),
-				Commands.waitSeconds(2),
-				sysIdQuasistatic(velocitySysIDRoutine, Direction.kReverse),
-				Commands.waitSeconds(2),
-				sysIdDynamic(velocitySysIDRoutine, Direction.kForward),
-				Commands.waitSeconds(2),
-				sysIdDynamic(velocitySysIDRoutine, Direction.kReverse),
-				Commands.runOnce(SignalLogger::stop)).handleInterrupt(SignalLogger::stop));
-	}
-
-	public static void setupSysId() {
-		velocitySysIDRoutine = new SysIdRoutine(
-				new SysIdRoutine.Config(
-						null, null,
-						Seconds.of(7), // Use default timeout (10 s)
-						// Log state with SignalLogger class
-						state -> SignalLogger.writeString("SysIdVelocity_State", state.toString())),
-				new SysIdRoutine.Mechanism(
-						output -> {
-							SignalLogger.writeDouble("Voltage", output.in(Volts));
-						},
-						null,
-						shooter));
-	}
-
-	/**
-	 * Runs the SysId Quasistatic test in the given direction for the routine
-	 * specified by {@link #m_sysIdRoutineToApply}.
-	 *
-	 * @param direction Direction of the SysId Quasistatic test
-	 * @return Command to run
-	 */
-	public static Command sysIdQuasistatic(SysIdRoutine routine, SysIdRoutine.Direction direction) {
-		return routine.quasistatic(direction);
-	}
-
-	/**
-	 * Runs the SysId Dynamic test in the given direction for the routine
-	 * specified by {@link #m_sysIdRoutineToApply}.
-	 *
-	 * @param direction Direction of the SysId Dynamic test
-	 * @return Command to run
-	 */
-	public static Command sysIdDynamic(SysIdRoutine routine, SysIdRoutine.Direction direction) {
-		return routine.dynamic(direction);
 	}
 }
