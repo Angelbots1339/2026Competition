8231288b79d4776b4da1195093dc75632fc60b80
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-02-09 10:52:53 -0700

    cleanup alignutil and properly use transform 2d
    apparantly just reinvented transformBy()

--- src/main/java/frc/lib/util/AlignUtil.java
+++ src/main/java/frc/lib/util/AlignUtil.java
@@ -4,10 +4,8 @@ import static edu.wpi.first.units.Units.Meters;
 
 import java.util.Arrays;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.RobotConstants;
 import frc.robot.subsystems.Swerve;
@@ -20,29 +18,17 @@ public class AlignUtil {
 	 * rotation = 0 = facing the pose directly with the front of the robot (the face
 	 * of the pose will face the robot directly)
 	 */
-	public static final Transform2d rightTowerOffset = new Transform2d(Meters.zero(),
+	public static final Transform2d leftTowerOffset = new Transform2d(Meters.zero(),
 			FieldUtil.towerWidth.div(2).minus(RobotConstants.climberOffset.getMeasureX()),
 			Rotation2d.kCCW_90deg);
-	public static final Transform2d leftTowerOffset = new Transform2d(Meters.zero(),
+	public static final Transform2d rightTowerOffset = new Transform2d(Meters.zero(),
 			FieldUtil.towerWidth.div(2).unaryMinus().plus(RobotConstants.climberOffset.getMeasureX()),
 			Rotation2d.kCW_90deg);
 
-	/* offsets a pose target with a robot relative offset */
-	public static Pose2d offsetPose(Pose2d target, Transform2d offset) {
-		// convert robot relative to blue origin coordinates
-		offset = new Transform2d(-offset.getX(), -offset.getY(), offset.getRotation().plus(Rotation2d.k180deg));
-		Translation2d tmp = offset.getTranslation();
-		tmp.rotateBy(target.getRotation());
-
-		target = target.plus(
-				new Transform2d(tmp.getX(), tmp.getY(), offset.getRotation()));
-		return target;
-	}
-
 	public static Command driveToTowerSide(Swerve swerve) {
 		return swerve.pidtoPose(
-				() -> swerve.getPose().nearest(Arrays.asList(offsetPose(FieldUtil.getTowerCenter(),
-						leftTowerOffset),
-						offsetPose(FieldUtil.getTowerCenter(), rightTowerOffset))));
+				() -> swerve.getPose().nearest(Arrays.asList(
+						FieldUtil.getTowerCenter().transformBy(leftTowerOffset),
+						FieldUtil.getTowerCenter().transformBy(rightTowerOffset))));
 	}
 }
