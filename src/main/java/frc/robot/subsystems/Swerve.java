9a6e3a953975514e3d3e816987ffd91eaaeaab46
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-02-09 19:13:37 -0700

    fix module-based pose returning opposite direction
    apparantly caused by disconnect between getPose and ctre internal pose

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -181,6 +181,7 @@ public class Swerve extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> impleme
 
 	public void setYaw(Rotation2d yaw) {
 		getPigeon2().setYaw(yaw.getDegrees());
+		resetRotation(yaw);
 	}
 
 	public void resetGyro() {
