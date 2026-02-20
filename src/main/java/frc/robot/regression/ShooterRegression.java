15953ac0be32f2c4f94eb600317e0d8f054cf1a1
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-02-20 15:54:48 -0700

    initial implementation of sotm (data pending)

--- src/main/java/frc/robot/regression/ShooterRegression.java
+++ src/main/java/frc/robot/regression/ShooterRegression.java
@@ -3,6 +3,8 @@ package frc.robot.regression;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Radians;
+
+import dev.doglog.DogLog;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -11,6 +13,7 @@ import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.math.interpolation.Interpolator;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.Angle;
 import frc.lib.util.FieldUtil;
 import frc.robot.subsystems.Swerve;
@@ -29,6 +32,13 @@ public class ShooterRegression {
 			{ 1.48, 41, 16 },
 	};
 
+	public static final double[][] tofData = {
+			// distance, tof from ball leaving the shooter to the hub
+			{ 1, 1 },
+			{ 2, 1.2 },
+			{ 5, 1.3 },
+	};
+
 	public static final InterpolatingDoubleTreeMap timeOfFlightMap = new InterpolatingDoubleTreeMap();
 
 	public static final InterpolatingTreeMap<Double, double[]> shotRPSMap = new InterpolatingTreeMap<Double, double[]>(
@@ -44,28 +54,53 @@ public class ShooterRegression {
 		for (double[] data : shotRPSData) {
 			shotRPSMap.put(data[0], new double[] { data[1], data[2] });
 		}
+		for (double[] data : tofData) {
+			timeOfFlightMap.put(data[0], data[1]);
+		}
 	}
 
 	public record ShooterParams(Rotation2d angle, double shooterRPS, double spinnerRPS, Angle maxAngleError) {
-
 	};
 
 	public static ShooterParams getShotParams(Swerve swerve) {
 		Translation2d target = FieldUtil.getHubCenter().getTranslation();
-		Pose2d estimatedPose = swerve.getPose();
-		double distance = target.getDistance(estimatedPose.getTranslation());
-		double[] rps = getRegressionRPS(distance);
+		Translation2d pose = swerve.getPose().getTranslation();
+		double distance = target.getDistance(pose);
+
+		ChassisSpeeds fieldSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(swerve.getRobotRelativeSpeeds(),
+				swerve.getYaw());
+		double vx = fieldSpeeds.vxMetersPerSecond;
+		double vy = fieldSpeeds.vyMetersPerSecond;
+
+		// When shooting, we are assuming the TOF of the ball being the same as when
+		// leaving the current pose.
+		// however, as we are shooting farther/closer depending on our
+		// velocity/lookahead pose, the actual TOF is shorter/longer
+		// iterate and use the TOF from the lookaheadpose to calculate a better
+		// lookahead pose
+		Translation2d lookaheadPose = pose;
+		double lookaheadDistance = distance;
+		double tof = 0;
+		for (int i = 0; i < 20; i++) {
+			tof = timeOfFlightMap.get(lookaheadDistance);
+			lookaheadPose = pose.plus(new Translation2d(vx * tof, vy * tof));
+			lookaheadDistance = target.getDistance(lookaheadPose);
+		}
+		DogLog.log("Regression/Lookahead Pose", new Pose2d(lookaheadPose, swerve.getYaw()));
+
+		double[] rps = shotRPSMap.get(lookaheadDistance);
 
 		Rotation2d angle = Rotation2d.fromRadians(
-				target.minus(estimatedPose.getTranslation()).getAngle().getRadians());
+				target.minus(lookaheadPose).getAngle().getRadians());
 
 		Angle maxAngleError = Radians
-				.of(Math.abs(Math.atan2(distance, FieldUtil.hubRadius.in(Meters))));
+				.of(Math.abs(Math.atan2(lookaheadDistance, FieldUtil.hubRadius.in(Meters))));
 
 		return new ShooterParams(angle, rps[0], rps[1], maxAngleError);
 	}
 
-	public static double[] getRegressionRPS(double distance) {
-		return shotRPSMap.get(distance);
+	public static double[] getRegressionRPS(double meters) {
+		return shotRPSMap.get(meters);
 	}
+
 }
