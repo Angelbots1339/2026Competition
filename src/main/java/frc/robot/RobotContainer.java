137d1049561ee2050c447e6af7afbb20800d6ae3
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-03-06 09:15:39 -0700

    increase pass velocity

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -104,7 +104,7 @@ public class RobotContainer {
 		pass.whileTrue(Commands.parallel(
 				swerve.run(() -> swerve.angularDriveRequest(leftY, leftX,
 						() -> FieldUtil.isRedAlliance() ? Rotation2d.kZero : Rotation2d.k180deg, () -> true)),
-				new Shoot(shooter, indexer, intake, () -> 30.0, () -> 30.0, swerve::atRotation)));
+				new Shoot(shooter, indexer, intake, () -> 45.0, () -> 45.0, swerve::atRotation)));
 		bumpDrive.whileTrue(
 				Commands.run(() -> swerve.angularDriveRequest(leftY, leftX, () -> swerve.getClosestBumpAngle(),
 						() -> true),
