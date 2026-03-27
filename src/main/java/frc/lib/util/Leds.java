8c073a58b5cd9024d05fe02a26edd8ab268051ee
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-03-27 08:40:00 -0600

    remove led reverse code
    will figure out this later, just going to merge this just initially

--- src/main/java/frc/lib/util/Leds.java
+++ src/main/java/frc/lib/util/Leds.java
@@ -31,6 +31,7 @@ public class Leds extends SubsystemBase {
 
 	private static Leds instance;
 	private final CANdle m_candle = new CANdle(60);
+	private static double globalTimer = 0.0;
 
 	private Alliance alliance = Alliance.Blue;
 
@@ -75,9 +76,7 @@ public class Leds extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-		for (int i = 0; i < buf.getLength(); i++) {
-			SmartDashboard.putString("LEDS/" + i, buf.getLED(i).toHexString());
-		}
+		globalTimer = Timer.getFPGATimestamp();
 		if (DriverStation.isFMSAttached() || DriverStation.isDSAttached()) {
 			driverStation_attached = true;
 		}
@@ -94,55 +93,53 @@ public class Leds extends SubsystemBase {
 			hubStateChangeAlert = false;
 		}
 
-		// wave(Section.TOP, defaultColor, Color.kBlack, waveSlowCycleLength,
-		// waveSlowPeriod);
-		//
-		// if (DriverStation.isDisabled()) {
-		// if (lowbattery || criticallyLowbattery) {
-		// if (criticallyLowbattery)
-		// strobe(Section.TOP, Color.kOrange, Hertz.of(3));
-		// else
-		// solid(Color.kOrange);
-		// } else {
-		// switch (alliance) {
-		// case Red:
-		// if (!driverStation_attached)
-		// strobe(Section.TOP, Color.kRed, DSAttachFreq);
-		// else
-		// breath(Section.TOP, Color.kRed, Color.kBlack, allianceBreathPeriod);
-		// break;
-		// case Blue:
-		// if (!driverStation_attached)
-		// strobe(Section.TOP, Color.kBlue, DSAttachFreq);
-		// else
-		// breath(Section.TOP, Color.kBlue, Color.kBlack, allianceBreathPeriod);
-		// break;
-		// default:
-		// if (!driverStation_attached)
-		// strobe(Section.TOP, defaultColor, DSAttachFreq);
-		// else
-		// wave(Section.TOP, defaultColor, Color.kBlack, waveSlowCycleLength,
-		// waveSlowPeriod);
-		// break;
-		// }
-		// }
-		// } else {
-		// solid(isHubActive ? Color.kGreen : Color.kRed);
-		// }
-		//
-		// if (shooting) {
-		// stripes(Section.TOP, List.of(Color.kBlack, Color.kRed), stripesLongLength,
-		// stripesFastPeriod, true);
-		solid(Section.RIGHT, Color.kRed);
-		// solid(Section.RIGHT, Color.kRed);
-		// }
+		wave(Section.TOP, defaultColor, Color.kBlack, waveSlowCycleLength,
+				waveSlowPeriod);
 
-		// if(hubStateChangeAlert)
-		//
-		// strobe(Section.TOP, isHubActive ? Color.kGreen : Color.kRed, hubAlertFreq);
+		if (DriverStation.isDisabled()) {
+			if (lowbattery || criticallyLowbattery) {
+				if (criticallyLowbattery)
+					strobe(Section.TOP, Color.kOrange, Hertz.of(3));
+				else
+					solid(Color.kOrange);
+			} else {
+				switch (alliance) {
+					case Red:
+						if (!driverStation_attached)
+							strobe(Section.TOP, Color.kRed, DSAttachFreq);
+						else
+							breath(Section.TOP, Color.kRed, Color.kBlack, allianceBreathPeriod);
+						break;
+					case Blue:
+						if (!driverStation_attached)
+							strobe(Section.TOP, Color.kBlue, DSAttachFreq);
+						else
+							breath(Section.TOP, Color.kBlue, Color.kBlack, allianceBreathPeriod);
+						break;
+					default:
+						if (!driverStation_attached)
+							strobe(Section.TOP, defaultColor, DSAttachFreq);
+						else
+							wave(Section.TOP, defaultColor, Color.kBlack, waveSlowCycleLength,
+									waveSlowPeriod);
+						break;
+				}
+			}
+		} else {
+			solid(isHubActive ? Color.kGreen : Color.kRed);
+		}
+
+		if (shooting) {
+			stripes(Section.TOP, List.of(Color.kRed, Color.kBlack), stripesLongLength, stripesFastPeriod);
+		}
+
+		if (hubStateChangeAlert)
+			strobe(Section.TOP, isHubActive ? Color.kGreen : Color.kRed, hubAlertFreq);
 
 		setLEDS();
-
+		for (int i = 0; i < buf.getLength(); i++) {
+			SmartDashboard.putString("LEDS/" + i, buf.getLED(i).toHexString());
+		}
 	}
 
 	public void setLEDS() {
@@ -174,12 +171,12 @@ public class Leds extends SubsystemBase {
 	}
 
 	public void strobe(Section section, Color color, double duration) {
-		boolean on = ((Timer.getFPGATimestamp() % duration) / duration) > 0.5;
+		boolean on = ((globalTimer % duration) / duration) > 0.5;
 		solid(section, on ? color : Color.kBlack);
 	}
 
 	private void breath(Section section, Color c1, Color c2, Time duration) {
-		breath(section, c1, c2, duration.in(Seconds), Timer.getFPGATimestamp());
+		breath(section, c1, c2, duration.in(Seconds), globalTimer);
 	}
 
 	private void breath(Section section, Color c1, Color c2, double duration, double timestamp) {
@@ -196,7 +193,7 @@ public class Leds extends SubsystemBase {
 	}
 
 	private void wave(Section section, Color c1, Color c2, double cycleLength, double duration) {
-		double x = (1 - ((Timer.getFPGATimestamp() % duration) / duration)) * 2.0 * Math.PI;
+		double x = (1 - ((globalTimer % duration) / duration)) * 2.0 * Math.PI;
 		double xDiffPerLed = (2.0 * Math.PI) / cycleLength;
 		for (int i = 0; i < section.end(); i++) {
 			x += xDiffPerLed;
@@ -220,22 +217,13 @@ public class Leds extends SubsystemBase {
 		stripes(section, colors, length, period.in(Seconds));
 	}
 
-	private void stripes(Section section, List<Color> colors, int length, Time period, boolean reverse) {
-		stripes(section, colors, length, period.in(Seconds), reverse);
-	}
-
 	private void stripes(Section section, List<Color> colors, int length, double duration) {
-		stripes(section, colors, length, duration, false);
-	}
-
-	private void stripes(Section section, List<Color> colors, int length, double duration, boolean reverse) {
-		int offset = (int) (Timer.getFPGATimestamp() % duration / duration * length * colors.size());
+		int offset = (int) (globalTimer % duration / duration * length * colors.size());
 		for (int i = section.start(); i < section.end(); i++) {
 			int colorIndex = (int) (Math.floor((double) (i - offset) / length) + colors.size()) % colors.size();
 			colorIndex = colors.size() - 1 - colorIndex;
 			solid(i, colors.get(colorIndex));
 		}
-
 	}
 
 	private static enum Section {
@@ -256,6 +244,7 @@ public class Leds extends SubsystemBase {
 			}
 		}
 
+		// exclusive
 		private int end() {
 			switch (this) {
 				case TOP:
