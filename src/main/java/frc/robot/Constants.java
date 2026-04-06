a51e3765953127f428233058e18fb3a2df487fb1
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-04-06 16:15:36 -0600

    update swerve rotation pid values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -66,10 +66,10 @@ public class Constants {
 
 		// has least amount of error, overshooting seems to be result of wheel slip on
 		// concrete
-		public static final double angularDriveKP = 5.3;
+		public static final double angularDriveKP = 5;
 		public static final double angularDriveKI = 0;
 		public static final double angularDriveKD = 0;
-		public static final double angularDriveKS = 0.12;
+		public static final double angularDriveKS = 0.17;
 		public static final double angularDriveKV = 0;
 		public static final SimpleMotorFeedforward angularDriveFeedforward = new SimpleMotorFeedforward(
 				angularDriveKS,
