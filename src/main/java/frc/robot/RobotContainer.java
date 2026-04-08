7cb456149bd3fd7f44bb452de794a835d6c3b183
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-04-07 20:13:32 -0600

    override disabled hub status when shooting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,6 +34,7 @@ import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Swerve;
+import frc.lib.util.Leds;
 
 @Logged
 public class RobotContainer {
@@ -105,7 +106,9 @@ public class RobotContainer {
 		pass.whileTrue(Commands.parallel(
 				swerve.run(() -> swerve.angularDriveRequest(leftY, leftX,
 						() -> FieldUtil.isRedAlliance() ? Rotation2d.k180deg : Rotation2d.kZero, () -> true)),
-				new Shoot(shooter, indexer, intake, () -> 46.0, () -> 40.0, swerve::atRotation)));
+				new Shoot(shooter, indexer, intake, () -> 46.0, () -> 40.0, swerve::atRotation),
+				Commands.runOnce(() -> Leds.getInstance().passing = true)))
+				.onFalse(Commands.runOnce(() -> Leds.getInstance().passing = false));
 		bumpDrive.whileTrue(
 				Commands.run(() -> swerve.angularDriveRequest(leftY, leftX, () -> swerve.getClosestBumpAngle(),
 						() -> true),
