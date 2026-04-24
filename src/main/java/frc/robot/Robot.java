96b1678ed2c4abc1b96dd79a04b62826ab46cd92
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-04-24 14:31:58 -0600

    initial robot.java commenting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,6 +26,14 @@ import frc.lib.util.Leds;
 import frc.lib.util.tuning.TuningManager;
 import frc.robot.Constants.VisionConstants;
 
+// Robot.java contains all the logic that will run on the robot when in 
+// different states / startup
+
+// this is an example of a logged class using Epilogue.  we can pass options
+// into the annotation, to change aspects such as importance, logging method,
+// name, etc.
+// in this specific example, we make everything in the robot class log everything
+// at the critical level
 @Logged(importance = Importance.CRITICAL)
 public class Robot extends TimedRobot {
 	private Command m_autonomousCommand = Commands.none();
@@ -41,22 +49,50 @@ public class Robot extends TimedRobot {
 	private final Timer disabledTimer = new Timer();
 
 	public Robot() {
+		// this instanciates our robot through the robotcontainer abstraction
+		// which contains all of our subsystems, commands, autos, and controller
+		// bindings
 		m_robotContainer = new RobotContainer();
 		// PathfindingCommand.warmupCommand().schedule();
+		// dog log is the declarative logging library to quickly log intermediate
+		// values/data in functions, methods, commands, etc
+		// we can set options such as whether or not to publish the data through NT
+		// (by default it does unless it is connected to FMS8, whether to log pdh/canbus data,
+		// or whether to log non-DogLog NT data to a log file
 		DogLog.setOptions(new DogLogOptions().withNtPublish(false).withLogExtras(false).withCaptureNt(true));
+		// doglog can optionally (by default) log using a separate thread to improve performance
+		// however, this thread could take time (3-5s) to start up and will
+		// incur the time start up penalty the first time DogLog.log() is called
 		// doglog log thread takes some time to start up so warm it up
 		DogLog.log("", "");
 
+		// this is what starts the log file writer (I think doglog does this as
+		// well, but I added doglog way after these and never removed it)
 		DataLogManager.start();
+		// this will log driverstation data (robot mode, disabled/enabled, etc)
+		// to the log file
 		DriverStation.startDataLog(DataLogManager.getLog());
+		// this configures epilogue which is the main logging library used
+		// by adding annotations (the @Logged) to classes, functions, fields, etc,
+		// we can automatically log all the values in such classes to NT Tables
+		// or whatever backend we specify
+		// the main configuration option to use is the config.backend, which can
+		// be used to log to NT (the default) or to the log file
+		// you can also configure the period of logging to either slow down or 
+		// quicken the logging
 		Epilogue.configure(config -> {
 			// config.minimumImportance = Importance.CRITICAL;
 			config.minimumImportance = Importance.DEBUG;
 			config.backend = new FileBackend(DataLogManager.getLog());
 		});
+		// this starts up epilogue
 		Epilogue.bind(this);
 		// DogLog.setEnabled(false);
 
+		// this is the match data we sent to the drivers to always show
+		// the reason these are separate calls is because logging to NT is very
+		// expensive and we are staggering all the logging calls and separating
+		// them into different periodic commands
 		addPeriodic(() -> DogLog.forceNt.log("Match/Shift Time Left", m_robotContainer.shifttimeleft()), 0.5);
 		addPeriodic(() -> DogLog.forceNt.log("Match/Hub Active", m_robotContainer.isHubActive()), 0.5);
 		addPeriodic(() -> DogLog.forceNt.log("Match/Hub Next Active", m_robotContainer.shiftNext()), 0.5);
