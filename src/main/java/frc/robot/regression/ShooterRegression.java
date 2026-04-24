ef310d166ac65f605a1c125c30c7af450fe29df1
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-04-24 16:06:15 -0600

    add comments on the shooterRegression.java

--- src/main/java/frc/robot/regression/ShooterRegression.java
+++ src/main/java/frc/robot/regression/ShooterRegression.java
@@ -19,6 +19,7 @@ import frc.robot.subsystems.Swerve;
 
 public class ShooterRegression {
 	public static final double minDistance = 2.0;
+	// this contains all the data used to map distance to shooter/spinner rps
 	public static final double[][] shotRPSData = {
 			// distance (m), shooter rps, spinner rps
 			// { 1.828, 41, 3 },
@@ -68,8 +69,15 @@ public class ShooterRegression {
 
 	};
 
+	// this is an interpolatingtree map which basically acts as a map/set
+	// in which if a value other than the valuse in the table are called, it interpolates
+	// from the two nearest data points to return a value
 	public static final InterpolatingDoubleTreeMap timeOfFlightMap = new InterpolatingDoubleTreeMap();
 
+	// this is the same thing as the timeofFlightMap but using a custom return value
+	// of a double[]
+	// we basically define a custom interpolation function used by the 
+	// interpolatingTreeMap class
 	public static final InterpolatingTreeMap<Double, double[]> shotRPSMap = new InterpolatingTreeMap<Double, double[]>(
 			InverseInterpolator.forDouble(), new Interpolator<double[]>() {
 				public double[] interpolate(double[] startValue, double[] endValue, double t) {
@@ -79,6 +87,9 @@ public class ShooterRegression {
 				};
 			});
 
+	// static {} blocks allow us to write code that runs in the class itself
+	// the reason we are separating the actual data and the interpolation maps
+	// is because it is easier to add data to the double[]
 	static {
 		for (double[] data : shotRPSData) {
 			shotRPSMap.put(data[0], new double[] { data[1], data[2] });
@@ -92,6 +103,8 @@ public class ShooterRegression {
 			boolean isValid) {
 	};
 
+	// this is the function that takes the swerves current position and velocity
+	// and returns the rps and angle of the robot to accurately shoot into the hub
 	public static ShooterParams getShotParams(Swerve swerve) {
 		Translation2d target = FieldUtil.getHubCenter().getTranslation();
 		Translation2d pose = swerve.getPose().getTranslation();
@@ -101,6 +114,14 @@ public class ShooterRegression {
 				swerve.getYaw());
 		double vx = fieldSpeeds.vxMetersPerSecond;
 		double vy = fieldSpeeds.vyMetersPerSecond;
+		// when shooting while the robot is moving, the robot's velocity is imparted
+		// on the ball, either slowing or quickining its velocity.
+		// as such, we are able target an offset position such that after shooting,
+		// the robot's imparted velocity accurately moves the ball to hit the target
+		//
+		// we do this by offsetting the target by the robot's current velocity vector
+		// scaled up by how long it takes for the ball to enter the hub after leaving
+		// the robot (the TOF)
 
 		// When shooting, we are assuming the TOF of the ball being the same as when
 		// leaving the current pose.
@@ -122,6 +143,8 @@ public class ShooterRegression {
 		Rotation2d angle = Rotation2d.fromRadians(
 				target.minus(lookaheadPose).getAngle().plus(Rotation2d.k180deg).getRadians());
 
+		// this angle is the maximum angle we can be off of before we have the center of the
+		// robot face away from the hub funnel
 		Angle maxAngleError = Radians
 				.of(Math.abs(Math.atan2(FieldUtil.hubRadius.in(Meters), distance)));
 
@@ -130,6 +153,11 @@ public class ShooterRegression {
 		DogLog.log("Regression/Angle", angle);
 		DogLog.log("Regression/Shooter RPS", rps[0]);
 		DogLog.log("Regression/Spinner RPS", rps[1]);
+		// basically don't allow the driver to shoot balls if we are too close to the robot
+		// looking back at this, this should probably be just distance, since the 
+		// blocker from shooting closer was the angle of our shot being too shallow
+		// and hitting the hub.  shooting from farther away would increase the angle
+		// of our shot and possibly allow for a theoritally sotm shot to hit inside
 		boolean isValid = lookaheadDistance >= minDistance;
 
 		return new ShooterParams(angle, rps[0], rps[1], maxAngleError, isValid);
